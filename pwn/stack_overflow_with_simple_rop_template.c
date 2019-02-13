27661b22532c11cf8fc95a5497a215403036127a
Marit Iren <maritiren@users.noreply.github.com>
2019-02-13 17:58:19 +0100

    Update stack_overflow_with_simple_rop_template.c

--- pwn/stack_overflow_with_simple_rop_template.c
+++ pwn/stack_overflow_with_simple_rop_template.c
@@ -7,14 +7,19 @@ void supersecret(void)
 	system("cat flag.txt");
 }
 
-int main(void) 
+void store_secret(void)
 {
 	char name[20];
 
 	setvbuf(stdout, NULL, _IONBF, 0);
 	printf("Welcome hackerman!\nPlease enter your secret password to go back in time: ");
 
-	read(STDIN_FILENO, name, 64);
+	read(STDIN_FILENO, name, 64);	
+}
 
+int main(void) 
+{
+	store_secret();
+	
 	return 0;
 }
